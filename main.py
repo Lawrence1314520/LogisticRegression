67a49aa3dfd625a830c969464a219481cc84dde4
Lawrence1314520 <1314520lihuahua@gmail.com>
2026-07-19 01:09:10 +0800

    updata to find the wrong prediction and output its id

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 
+import numpy as np
 from LogisticRegression import LogisticRegression
 from sklearn.model_selection import train_test_split
 from utils import load_data,normalize
 
-X,y=load_data("breast-cancer.csv")
+X,y,ids=load_data("breast-cancer.csv")
 
 X = normalize(X)
 
-X_train, X_test, y_train, y_test = train_test_split(
+X_train, X_test, y_train, y_test,id_train,id_test=train_test_split(
     X, 
     y,
+    ids,
     test_size=0.2,
     random_state=42
     )
@@ -22,7 +24,15 @@ probabilities=model.predict_probability(X_test)
 prediction=(probabilities>=0.5).astype(int)
 
 accuracy=(prediction==y_test).mean()
+np.set_printoptions(suppress=True, precision=4)
 
-print(f"Accuracy: {accuracy:.4f}")
-print("Prediction: ",prediction)
-print("Probability: ",probabilities)
+for i in range(len(prediction)):
+    if prediction[i] != y_test[i]:
+        print(f"Patient ID:      {id_test[i]}")
+        print(f"Sample:          {i}")
+        print(f"True label:      {y_test[i]}")
+        print(f"Probability:     {probabilities[i] * 100:.2f}%")
+        print(f"Prediction:      {prediction[i]}")
+        print("-" * 40)
+
+
--- utils.py
+++ utils.py
@@ -7,12 +7,14 @@ def load_data(file_name):
 
     data["diagnosis"]=data["diagnosis"].map({"M":1,"B":0})
 
+    ids=data["id"].to_numpy()
     X=data.drop(columns=["id","diagnosis"])
     y=data["diagnosis"]
+
     X=X.to_numpy()
     y=y.to_numpy()
 
-    return X,y
+    return X,y,ids
 
 def normalize(X):
    return  (X-X.mean(axis=0))/X.std(axis=0)
